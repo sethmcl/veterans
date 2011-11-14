24404b708ef9bc8fe920bef46c611fb45e78db8f
Seth McLaughlin <s@sethmcl.com>
2011-11-14 14:34:03 -0800

    Committing changes from weekend

--- js/app.js
+++ js/app.js
@@ -1,29 +1,43 @@
 define(function(require) {
+  var SignInView          = require('views/SignInView'),
+      LoggedInView        = require('views/LoggedInView'),      
+      peopleSearch        = require('search/peoplesearch'),
+      channel             = require('util/channel');  
+  
   var views = {};
   
   //Initialize the application
-  function initialize() {    
-    var SignInView      = require('views/sign-in'),
-        LoggedInView    = require('views/logged-in');
-
+  function initialize() {        
+    
     views['sign-in']    = new SignInView();
-    views['logged-in']  = new LoggedInView();
+    views['logged-in']  = new LoggedInView();    
 
     //Check if user is already logged in to LinkedIn and authorized for our app
     IN.Event.on(IN, 'frameworkLoaded', function() {
       if(IN.User.isAuthorized()) {
-        views['logged-in'].render();
+        onLinkedInAuth();
       } else {
         views['sign-in'].render();
       }
     });
 
     //Get notified when the user authorizes our app on LinkedIn
-    IN.Event.on(IN, 'auth', function() {
-      views['sign-in'].unrender();
-      views['logged-in'].render();
-    });
+    IN.Event.on(IN, 'auth', onLinkedInAuth);
     
+  }
+
+  //Render logged-in view
+  function onLinkedInAuth() {
+    var fields = ['firstName', 'headline', 'positions:(company)'];
+
+    IN.API.Profile('me')
+      .fields(fields)
+      .result(function(data) {
+        channel.pub('auth', 'user-data', data.values[0]);
+
+        views['sign-in'].unrender();
+        views['logged-in'].render();
+      });
   }
 
   return {

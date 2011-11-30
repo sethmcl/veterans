636d9f98da02553a7c6002c10b0ac5154e888df8
Seth McLaughlin <s@sethmcl.com>
2011-11-29 16:32:31 -0800

    added url customizations

--- js/app.js
+++ js/app.js
@@ -31,15 +31,38 @@ define(function(require) {
   function onLinkedInAuth() {
     var hash = window.location.hash.replace('#', '');
     var facets = ['location', 'industry', 'current-company'];
+    var parts = hash.split('&');
+    var urlParams = {};
 
-    if(hash.length > 0) {
-      facets = hash.split(',');
-    }
+    var cats = {
+      'army' : [1223, 1226],
+      'navy' : [1335],
+      'airforce' : [157235],
+      'marines' : [2039],
+      'coastguard' : [4425]
+    };
+
+    var pastCompanies = [1223, 1335, 2039, 4425, 1226, 157235, 1224, 1230, 1333, 1343, 1938783, 1400617, 1592500, 1233200, 2061386, 462659, 1022991, 1877670, 377572, 2010213, 1061746];
     
+    for(var i = 0, len = parts.length; i < len; i++) {
+      var pair = parts[i].split('=');
+      urlParams[pair[0]] = pair[1];
+    }
+
+    if(urlParams.f) {
+      facets = urlParams.f.split(',');
+    }
+
+    if(urlParams.p) {
+      //pastCompanies = urlParams.p.split(',');
+      if(cats[urlParams.p]) {
+        pastCompanies = cats[urlParams.p];
+      }
+    }
 
     peopleSearchInput.addFacets(facets, true);
     peopleSearchInput.set({      
-      'cohorts-past-company'  : [1223, 1335, 2039, 4425, 1226, 157235, 1224, 1230, 1333, 1343, 1938783, 1400617, 1592500, 1233200, 2061386, 462659, 1022991, 1877670, 377572, 2010213, 1061746],
+      'cohorts-past-company'  : pastCompanies,
       'keywords'              : 'united states',
       'sort'                  : 'distance'
     }); 
